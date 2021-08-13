b7d4aa828a66ec4fb9364dc18a21b506c97086d0
Nadwey <81181783+Nadwey@users.noreply.github.com>
2021-08-13 19:50:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,64 +2,37 @@
 #include <time.h>
 #include <random>
 
-std::string input;
-
-
 int random(int min, int max)
 {
-  return rand()%(max-min + 1) + min;
+    return rand()%(max-min + 1) + min;
 }
 
-void generate() {
+void generate()
+{
+    char literki[] = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+    char literki2[] = { 'a', 'e', 'i', 'o', 'u'};
 
-    std::string literki[21] = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
     
-    std::string literki2[5] = { "a", "e", "i", "o", "u"};
-
-    
-    srand(time(0));
     int losowanko = random(0, 20);
     int losowanko2 = random(0, 4);
     int losowanko3 = random(0, 20);
-    int losowanko4 = random(0, 4);
-    
     
 
     std::cout << literki[losowanko];
-
     std::cout << literki2[losowanko2];
-
     std::cout << literki[losowanko3];
-
-    std::cout << literki2[losowanko4];
-
-    std::string way_czy_wey[2] = { "way", "wey" };
-
-    int losowanko5 = random(0, 1);
-    std::cout << way_czy_wey[losowanko5] << std::endl;
-    
-
+  
+    std::cout << (random(0, 1) ? "wey" : "way") << std::endl;
 }
-
 
 int main()
 {
+    srand(time(0));
     std::cout << "Generator nazw dla nadweya" << std::endl;
-    std::cout << "Wpisz 'generate' aby wygenerowac nazwe dla midwaya"
-    << std::endl;
-
         
-    std::cin >> input;
-        
-    if (input == "generate") {
-            
-        std::cout << "Wygenerowana nazwa: " << std::endl;
-        generate();
-        
-    }
+    std::cout << "Wygenerowana nazwa: " << std::endl;
+    generate();
 
     
     return 0;
 }
-
-
